8395dd41bd663a825ec30ac5e4e48ccaf3675048
Elena Gontovaya <lena_tuman@mail.ru>
2022-08-11 09:15:21 +0300

    HW_sem_1_final

--- HW_sem1/Program.cs
+++ HW_sem1/Program.cs
@@ -5,22 +5,22 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-// Console.WriteLine ("Введите первое число");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine ("Введите второе число");
-// int num2 = Convert.ToInt32(Console.ReadLine());
-// if (num1>num2)
-// {
-//     Console.WriteLine ("Число " + num1 + " больше чем " + num2);
-// }
-// if (num1==num2)
-// {
-//     Console.WriteLine ("Вы ввели два одинаковых числа");
-// }
-// else
-// {
-//     Console.WriteLine ("Число " + num2 + " больше чем " + num1);
-// }
+Console.WriteLine ("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine ("Число " + num1 + " больше чем " + num2);
+}
+if (num1==num2)
+{
+    Console.WriteLine ("Вы ввели два одинаковых числа");
+}
+else
+{
+    Console.WriteLine ("Число " + num2 + " больше чем " + num1);
+}
 
 // --------------------------------------------------------------------------------
 
