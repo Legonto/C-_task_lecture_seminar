7b0d4bf42d44b801595fcc7cde5ce6655eb718e6
Elena Gontovaya <lena_tuman@mail.ru>
2022-09-01 10:14:03 +0300

    HW_sem7_final

--- HW_sem7/Program.cs
+++ HW_sem7/Program.cs
@@ -5,21 +5,21 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-// int m = Int32.Parse(Console.ReadLine());
-// int n = Int32.Parse(Console.ReadLine());
-// double [,] matrix = new double[m,n];
+int m = Int32.Parse(Console.ReadLine());
+int n = Int32.Parse(Console.ReadLine());
+double [,] matrix = new double[m,n];
 
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//         matrix[i,j] =new Random().NextDouble() * 20 - 10; 
-//         // тк NextDouble выдает числа от 0 до 1, решила умножить на 20 
-//         // и чтобы некоторые отрицательные получились, то - 10
-//         Console.Write("{0,6:F1}", matrix[i,j]);
-//     }
-//     Console.WriteLine();
-// }
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] =new Random().NextDouble() * 20 - 10; 
+        // тк NextDouble выдает числа от 0 до 1, решила умножить на 20 
+        // и чтобы некоторые отрицательные получились, то - 10
+        Console.Write("{0,6:F1}", matrix[i,j]);
+    }
+    Console.WriteLine();
+}
 
 
 //------------------------------------------------------------------
@@ -60,13 +60,29 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int m = Int32.Parse(Console.ReadLine());
-int n = Int32.Parse(Console.ReadLine());
-int [,] matrix = new int[m,n];
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        matrix[i,j] =new Random().Next();
-    }
-}
+// Console.WriteLine("Введите количество строк");
+// int m = Int32.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Int32.Parse(Console.ReadLine());
+// int [,] matrix = new int [m,n];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j] = new Random().Next(1,10);
+//         Console.Write("{0,2} ", matrix[i, j]);
+//     }
+//     Console.WriteLine();
+// }
+
+
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         sum += matrix[i, j];
+//     }
+//     Console.Write($"{Math.Round(sum / matrix.GetLength(0), 2)}; ");
+// }
+
